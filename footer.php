bd755e8bfca2b1225e55915b5ec7cddd12a4da40
MartInDurgi <martindurgala@gamil.com>
2023-06-06 13:26:24 +0200

    footer izmena

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-<footer>
-    <hr>
-    <p>All rights reserved <?php echo date('Y'); ?></p>
+<footer class="blog-footer">
+    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>
+        <a href="#">Back to top</a>
+    </p>
 </footer>
